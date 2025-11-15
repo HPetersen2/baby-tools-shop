a9e483f8d8b8c36481c286166fdb7677d29bffff
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-11-15 16:01:24 +0100

    add dockerfile with python 3.9 alpine

---
+++ babyshop_app/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+python manage.py makemigrations
+python manage.py migrate
+
+# Create a superuser using environment variables
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
+
+if not User.objects.filter(username=username).exists():
+    print(f"Creating superuser '{username}'...")
+    # Korrekter Aufruf: username hier übergeben
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' created.")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
+
+python manage.py &
+
+exec gunicorn babyshop.wsgi:application --bind 0.0.0.0:8025 --reload
